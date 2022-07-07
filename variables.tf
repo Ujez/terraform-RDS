9a9f5a567f793a8a8103d3dd30251edacc1968d2
ujez <ujezujez@gmail.com>
2022-07-07 16:09:47 +0100

    little update

--- variables.tf
+++ variables.tf
@@ -9,6 +9,16 @@ variable "AWS_REGION" {
   default = "us-east-1"
 }
 
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-0f65ab0fd913bc7be"
+    us-east-2 = "ami-0d4a68c63d9d1d72d"
+    us-west-2 = "ami-0688ba7eeeeefe3cd"
+    us-west-1 = "ami-0454207e5367abf01"
+  }
+}
+
 variable "PATH_TO_PRIVATE_KEY" {
   default = "levelup_key"
 }
